ca9fa7ef9b8d670ff7cf918b71100ce69df3ddda
Igor <zalugin.iv2@gmail.com>
2026-02-10 15:38:08 +0100

    Requested changes completed, flake8 changes.

--- app/main.py
+++ app/main.py
@@ -10,9 +10,9 @@ def format_linter_error(error: dict) -> dict:
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
     return {
-    "errors": [] if not errors else
-    [format_linter_error(e) for e in errors],
-    "path": file_path, "status": "passed" if not errors else "failed"
+        "errors": [] if not errors else
+        [format_linter_error(e) for e in errors],
+        "path": file_path, "status": "passed" if not errors else "failed"
     }
 
 
