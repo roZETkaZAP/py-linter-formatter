ea815d5843e27e05c8e7141fdafdf6fa226ae584
Igor <zalugin.iv2@gmail.com>
2026-02-10 16:15:29 +0100

    Requested changes completed

--- app/main.py
+++ app/main.py
@@ -1,3 +1,96 @@
+errors = [
+    {"errors": [], "path": "./test_source_code_2.py", "status": "passed"},
+    {
+        "errors": [
+            {
+                "line": 18,
+                "column": 80,
+                "message": "line too long (99 > 79 characters)",
+                "name": "E501",
+                "source": "flake8",
+            },
+            {
+                "line": 18,
+                "column": 100,
+                "message": "no newline at end of file",
+                "name": "W292",
+                "source": "flake8",
+            },
+        ],
+        "path": "./source_code_2.py",
+        "status": "failed",
+    },
+    {
+        "errors": [
+            {
+                "line": 3,
+                "column": 74,
+                "message": "multiple statements on one line (semicolon)",
+                "name": "E702",
+                "source": "flake8",
+            },
+            {
+                "line": 3,
+                "column": 80,
+                "message": "line too long (97 > 79 characters)",
+                "name": "E501",
+                "source": "flake8",
+            },
+            {
+                "line": 15,
+                "column": 1,
+                "message": "expected 2 blank lines, found 1",
+                "name": "E302",
+                "source": "flake8",
+            },
+            {
+                "line": 27,
+                "column": 1,
+                "message": "too many blank lines (6)",
+                "name": "E303",
+                "source": "flake8",
+            },
+            {
+                "line": 31,
+                "column": 80,
+                "message": "line too long (99 > 79 characters)",
+                "name": "E501",
+                "source": "flake8",
+            },
+        ],
+        "path": "./source_code_1.py",
+        "status": "failed",
+    },
+    {
+        "errors": [
+            {
+                "line": 4,
+                "column": 1,
+                "message": "expected 2 blank lines, found 1",
+                "name": "E302",
+                "source": "flake8",
+            },
+            {
+                "line": 32,
+                "column": 80,
+                "message": "line too long (84 > 79 characters)",
+                "name": "E501",
+                "source": "flake8",
+            },
+            {
+                "line": 112,
+                "column": 6,
+                "message": "no newline at end of file",
+                "name": "W292",
+                "source": "flake8",
+            },
+        ],
+        "path": "./test_source_code_1.py",
+        "status": "failed",
+    },
+]
+
+
 def format_linter_error(error: dict) -> dict:
     return {
         "line": error["line_number"],
